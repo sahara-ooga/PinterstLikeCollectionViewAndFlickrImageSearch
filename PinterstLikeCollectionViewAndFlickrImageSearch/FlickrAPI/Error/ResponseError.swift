22905ea2cd1be39fd4d0b6dc1562dddfab555108
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-10-30 17:54:34 +0900

    エラー型の追加

---
+++ PinterstLikeCollectionViewAndFlickrImageSearch/FlickrAPI/Error/ResponseError.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseError.swift
+//  PinterstLikeCollectionViewAndFlickrImageSearch
+//
+//  Created by Yuu Ogasa on 2017/10/30.
+//  Copyright © 2017年 SundayCarpenter. All rights reserved.
+//
+
+import Foundation
+
+struct ResponseError:Error {
+    let message:String
+}
