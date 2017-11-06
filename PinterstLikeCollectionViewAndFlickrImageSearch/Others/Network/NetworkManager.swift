613a0f2d9c67334ab57199aa11918e27d3fd907a
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-11-06 10:49:56 +0900

    Add Network manager

---
+++ PinterstLikeCollectionViewAndFlickrImageSearch/Others/Network/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  ios-flickr-api-demo
+//
+//  Created by Eiji Kushida on 2016/12/18.
+//  Copyright © 2016年 Eiji Kushida. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkManager {
+
+    static func isAvailable() -> Bool{
+
+        let net = NetworkReachabilityManager()
+        net?.startListening()
+
+        if net?.isReachable ?? false {
+            return true
+
+        } else {
+            return false
+        }
+    }
+}
